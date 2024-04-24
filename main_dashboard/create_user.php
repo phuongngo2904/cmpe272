f3f4a91425d4f66ab681fd384887334e55def887
Phuong <phuong@Phuong.local>
2024-04-23 20:33:12 -0700

    bug fix

--- main_dashboard/create_user.php
+++ main_dashboard/create_user.php
@@ -1,28 +1,38 @@
 <?php
-    $servername = "localhost";
-    $username = "dev";
-    $password = "dev";
-    $dbname = "cmpe272";
+$servername = "localhost";
+$username = "dev";
+$password = "dev";
+$dbname = "cmpe272";
 
+try {
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
+
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $home_address = $_POST['home_address'];
     $home_phone = $_POST['home_phone'];
     $cell_phone = $_POST['cell_phone'];
+
     $sql = "INSERT INTO user_table (first_name, last_name, email, home_address, home_phone, cell_phone)
             VALUES ('$first_name', '$last_name', '$email', '$home_address', '$home_phone', '$cell_phone')";
 
     if ($conn->query($sql) === TRUE) {
         echo "User created successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        if ($conn->errno == 1062) {
+            echo "Error: User $first_name  $last_name already exists.";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
 
     $conn->close();
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
 ?>
